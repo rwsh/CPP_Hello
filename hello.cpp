97676bf2285229909bc66de9be247353abe2d51e
Roman Shamin <roman@shamin.ru>
2022-01-02 08:57:23 +0300

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // подключение библиотеки ввода/вывода
+#include <cmath> // математическая библотека
+
+using namespace std; // подключение пространства имен 
+
+int main()
+{
+    cout << "Hello, World!" << endl; // выводим приветствие
+    cout << log(2.0) << endl; // выводим логарифм
+}
